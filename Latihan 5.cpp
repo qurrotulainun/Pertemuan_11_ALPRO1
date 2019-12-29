490404e0704a2fed3635b238cda0efb19af29bb0
Qurrotul Ainun Ni'matus Sholihah <59301024+qurrotulainun@users.noreply.github.com>
2019-12-29 22:02:40 +0700

    Iterasi 2 = Bilangan

---
+++ Latihan 5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//Latihan 5 Iterasi 2 = Bilangan
+using namespace std;
+main()
+{
+	int n,k;
+	cout<<"Masukkan sebuah angka = ";cin>>k;
+	for (n=0;n<=k;n++)
+	cout<<"angka = "<<n<<endl;
+}
