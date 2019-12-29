e049536682746247533f0e3b04133f044d285084
Qurrotul Ainun Ni'matus Sholihah <59301024+qurrotulainun@users.noreply.github.com>
2019-12-29 22:02:03 +0700

    Rekursif 3 = Bilangan Fibonacci

---
+++ Latihan 4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+//Latihan 4 Rekursif 3 = Bilangan Fibonacci
+using namespace std;
+int fibonacci(int n)
+{
+	if ( n == 0 || n == 1)
+	{
+		return n;
+	}
+	else 
+	{
+		return(fibonacci(n-1) + fibonacci(n-2));
+	}
+}
+int main()
+{
+	int n, i, j=0;
+	cout << "Masukkan batas jumlah bilangan fibonancci: ";
+	cin >> n;
+	cout << "Hasil bilangan fibonacci: \n";
+	for (i=1;i<=n;i++)
+	{
+		cout << "fibonacci ke - "<< i <<" : " << fibonacci(j) << endl;
+		j++;
+	}
+	return 0;
+}
